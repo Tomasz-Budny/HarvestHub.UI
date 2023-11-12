5139500d79ebd5ee625a13ecafd30c50f4652706
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-12 15:47:26 +0100

    Add navbar component

--- src/app/dashboard/feature/dashboard.component.ts
+++ src/app/dashboard/feature/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MapComponent } from './map/map.component';
 import { SidePanelComponent } from './side-panel/side-panel.component';
+import { NavbarComponent } from '../../shared/feature/navbar/navbar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MapComponent, SidePanelComponent],
+  imports: [MapComponent, SidePanelComponent, NavbarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
