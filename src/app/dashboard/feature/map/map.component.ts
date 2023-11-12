353748994a93519a58f01d4bfa05f7ae01f375de
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-12 13:42:50 +0100

    install google maps package, add googleMaps module in map component

--- src/app/dashboard/feature/map/map.component.ts
+++ src/app/dashboard/feature/map/map.component.ts
@@ -1,10 +1,23 @@
+import { AsyncPipe } from '@angular/common';
+import { HttpClient, HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { Observable, catchError, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-map',
   standalone: true,
+  imports: [GoogleMapsModule, HttpClientModule, HttpClientJsonpModule, AsyncPipe],
   templateUrl: './map.component.html'
 })
 export class MapComponent {
-  
+  apiLoaded: Observable<boolean>;
+
+  constructor(httpClient: HttpClient) {
+    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyBuKv27xR4mZj_nWp6ljbbo0x_ta0yrui4', 'callback')
+        .pipe(
+          map(() => true),
+          catchError(() => of(false)),
+        );
+  }
 }
