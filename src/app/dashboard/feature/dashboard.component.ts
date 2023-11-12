2e296efd58344fcfdfc45e7747235017679ba0fd
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-12 13:12:28 +0100

    Remove unused imports

--- src/app/dashboard/feature/dashboard.component.ts
+++ src/app/dashboard/feature/dashboard.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { MapComponent } from './map/map.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MapComponent],
+  imports: [MapComponent],
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent {
