2592f4543337cb785872bb1d23551ebcb3596b22
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-12 14:00:26 +0100

    install missing dependencies add basic map config

--- src/app/dashboard/feature/map/map.component.ts
+++ src/app/dashboard/feature/map/map.component.ts
@@ -1,8 +1,8 @@
 import { AsyncPipe } from '@angular/common';
 import { HttpClient, HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMap, GoogleMapsModule } from '@angular/google-maps';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-map',
@@ -11,13 +11,28 @@ import { Observable, catchError, map, of } from 'rxjs';
   templateUrl: './map.component.html'
 })
 export class MapComponent {
+  @ViewChild(GoogleMap) map: GoogleMap;
   apiLoaded: Observable<boolean>;
+  options: google.maps.MapOptions;
+  center: google.maps.LatLngLiteral;
 
   constructor(httpClient: HttpClient) {
     this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyBuKv27xR4mZj_nWp6ljbbo0x_ta0yrui4', 'callback')
         .pipe(
           map(() => true),
           catchError(() => of(false)),
+          tap({
+            next: _ => this.initializeMap()
+          })
         );
+  }
+
+  initializeMap() {
+    this.options = {
+      disableDefaultUI: true,
+      fullscreenControl: true,
+      mapTypeId: google.maps.MapTypeId.HYBRID,
+    }
+    this.center = {lat: 53.0518, lng: 20.703};
   }
 }
