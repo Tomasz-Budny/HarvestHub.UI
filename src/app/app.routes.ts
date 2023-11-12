d0ef354f1e3ef9610cabd4270fbdd03c5c210806
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-12 13:05:41 +0100

    Remove default files and contents, add dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ { path: 'dashboard', loadComponent: () => import('./dashboard/feature/dashboard.component').then(mod => mod.DashboardComponent) },
+ { path: '**', redirectTo: 'dashboard' }
+];
