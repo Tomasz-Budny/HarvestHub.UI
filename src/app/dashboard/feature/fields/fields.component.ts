422ac79f3ca930b5f8c6f038a196f6efede1042d
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-14 23:34:08 +0100

    Add foo structure of field component

--- src/app/dashboard/feature/fields/fields.component.ts
+++ src/app/dashboard/feature/fields/fields.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FieldComponent } from '../../ui/field/field.component';
 
 @Component({
   selector: 'app-fields',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FieldComponent],
   templateUrl: './fields.component.html',
   styleUrl: './fields.component.scss'
 })
---
+++ src/app/dashboard/ui/field/field.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-field',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './field.component.html',
+  styleUrl: './field.component.scss'
+})
+export class FieldComponent {
+
+}
