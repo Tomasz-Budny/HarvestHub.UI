d32aff7d84c5d582b34c509fdbb409d331998151
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-14 19:37:10 +0100

    Add copyright component

--- src/app/dashboard/feature/side-panel/side-panel.component.ts
+++ src/app/dashboard/feature/side-panel/side-panel.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherForecastComponent } from '../weather-forecast/weather-forecast.component';
+import { CopyrightComponent } from '../../ui/copyright/copyright.component';
 
 @Component({
   selector: 'app-side-panel',
   standalone: true,
-  imports: [CommonModule, WeatherForecastComponent],
+  imports: [CommonModule, WeatherForecastComponent, CopyrightComponent],
   templateUrl: './side-panel.component.html',
   styleUrl: './side-panel.component.scss'
 })
---
+++ src/app/dashboard/ui/copyright/copyright.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-copyright',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './copyright.component.html',
+  styleUrl: './copyright.component.scss'
+})
+export class CopyrightComponent {
+
+}
