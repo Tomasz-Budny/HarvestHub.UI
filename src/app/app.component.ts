29d1d003e9a629bb17da8e9f8f0376059dee8488
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-14 16:45:26 +0100

    Add angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatIconRegistry } from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(
+    private matIconRegistry: MatIconRegistry
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      '', 
+      ''
+    );
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom([HttpClientModule, HttpClientJsonpModule]),
-    provideRouter(routes)
+    importProvidersFrom([HttpClientModule, HttpClientJsonpModule, MatIconModule]),
+    provideRouter(routes),
+    provideAnimations()
   ]
 };
