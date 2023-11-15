d023bab0fc6232b2a7ffde6916b1d2edb69ba364
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-15 18:31:44 +0100

    Add foo data to fields-area-chart

--- src/app/dashboard/ui/fields-area-chart/fields-area-chart.component.ts
+++ src/app/dashboard/ui/fields-area-chart/fields-area-chart.component.ts
@@ -12,12 +12,20 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
 export class FieldsAreaChartComponent {
   title = 'barchartApp';
   customColors = [
-    { name: "X", value: '#324C08' },
-    { name: "Y", value: '#E6E5A3' }
+    { name: "Działka #1", value: '#324C08' },
+    { name: "Działka #2", value: '#E6E5A3' },
+    { name: "Działka #3", value: '#856035' },
+    { name: "Działka #4", value: '#DAC92E' },
+    { name: "Działka #5", value: '#647D3B' },
+    { name: "Działka #6", value: '#C1C35D' }
   ];
 
   dataset = [
-    { name: "X", value: 1 },
-    { name: "Y", value: 2 }
+    { name: "Działka #1", value: 3.14 },
+    { name: "Działka #2", value: 5.64 },
+    { name: "Działka #3", value: 9.25 },
+    { name: "Działka #4", value: 5.01 },
+    { name: "Działka #5", value: 7.57 },
+    { name: "Działka #6", value: 8.01 },
   ];
 }
