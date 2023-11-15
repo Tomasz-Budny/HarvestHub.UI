c1a4bf25147aeee8aff31e953bca563d59293367
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-15 17:47:34 +0100

    Add ngx-charts package

--- src/app/dashboard/feature/side-panel/side-panel.component.ts
+++ src/app/dashboard/feature/side-panel/side-panel.component.ts
@@ -3,14 +3,19 @@ import { CommonModule } from '@angular/common';
 import { WeatherForecastComponent } from '../weather-forecast/weather-forecast.component';
 import { CopyrightComponent } from '../../ui/copyright/copyright.component';
 import { FieldsComponent } from '../fields/fields.component';
+import { NgxChartsModule }from '@swimlane/ngx-charts';  // added
 
 @Component({
   selector: 'app-side-panel',
   standalone: true,
-  imports: [CommonModule, WeatherForecastComponent, CopyrightComponent, FieldsComponent],
+  imports: [CommonModule, WeatherForecastComponent, CopyrightComponent, FieldsComponent, NgxChartsModule],
   templateUrl: './side-panel.component.html',
   styleUrl: './side-panel.component.scss'
 })
 export class SidePanelComponent {
-
+  title = 'barchartApp';
+  dataset = [
+    { name: "X", value: 1 },
+    { name: "Y", value: 2 }
+  ];
 }
