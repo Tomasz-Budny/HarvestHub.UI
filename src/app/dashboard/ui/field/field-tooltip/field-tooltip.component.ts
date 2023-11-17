07c5b34a410bac150572514f707b825a80972456
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-17 17:09:19 +0100

    Add field tooltip component

---
+++ src/app/dashboard/ui/field/field-tooltip/field-tooltip.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-field-tooltip',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './field-tooltip.component.html',
+  styleUrl: './field-tooltip.component.scss'
+})
+export class FieldTooltipComponent {
+
+}
