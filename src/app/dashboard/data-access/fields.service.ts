b1f6f26b04289cd425430a2b4ae8b964143d7640
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-17 15:20:55 +0100

    connect fields with backend

--- src/app/dashboard/data-access/fields.service.ts
+++ src/app/dashboard/data-access/fields.service.ts
@@ -3,18 +3,23 @@ import { FieldViewModel } from '../data-model/field.model';
 import { NEVER, delay, of } from 'rxjs';
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { HarvestHubResponse } from '../../shared/data-model/harvest-hub-response.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FieldsService {
+  URL = 'https://localhost:7258/api/fields';
+
   private state = signal<HarvestHubResponse<FieldViewModel[]>>({
     data: [],
     loaded: false,
     error: null
   });
 
-  constructor() { 
+  constructor(
+    public http: HttpClient
+  ) { 
     this.loadFields().subscribe({
       next: res => this.state.update(state => ({
         ...state,
@@ -33,22 +38,8 @@ export class FieldsService {
   }
 
   loadFields() {
-    const fields = [
-      {name: 'Działka #1', area: 3.17, address: 'Żebry Kordy', color: '#324C08'},
-      {name: 'Działka #2', area: 4.01, address: 'Żebry Kordy', color: '#E6E5A3'},
-      {name: 'Działka #3', area: 6.17, address: 'Żebry Kordy', color: '#856035'},
-      {name: 'Działka #4', area: 3.23, address: 'Żebry Kordy', color: '#DAC92E'},
-      {name: 'Działka #5', area: 8.02, address: 'Żebry Kordy', color: '#647D3B'},
-      {name: 'Działka #6', area: 4.01, address: 'Żebry Kordy', color: '#E6E5A3'},
-      {name: 'Działka #7', area: 6.17, address: 'Żebry Kordy', color: '#856035'},
-      {name: 'Działka #8', area: 3.23, address: 'Żebry Kordy', color: '#DAC92E'},
-      {name: 'Działka #9', area: 8.02, address: 'Żebry Kordy', color: '#647D3B'},
-      {name: 'Działka #10', area: 1.00, address: 'Żebry Kordy', color: '#C1C35D'},
-    ];
-
-    return of(fields).pipe(
-      delay(1000),
+    return this.http.get<FieldViewModel[]>(this.URL).pipe(
       takeUntilDestroyed()
-    )
+    );
   }
 }
