7339e36f95a2b51d28d19284fa61becd6ad98cd0
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-19 12:11:48 +0100

    Add field button component

---
+++ src/app/dashboard/ui/add-field-button/add-field-button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-add-field-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-field-button.component.html',
+  styleUrl: './add-field-button.component.scss'
+})
+export class AddFieldButtonComponent {
+
+}
