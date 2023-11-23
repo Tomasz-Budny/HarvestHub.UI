fde85433a99e49088c0199b9107c627636a6ec62
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-23 12:48:33 +0100

    Add basic field details structure

--- src/app/dashboard/feature/field-details/field-details.component.ts
+++ src/app/dashboard/feature/field-details/field-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-field-details',
@@ -10,8 +10,18 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrl: './field-details.component.scss'
 })
 export class FieldDetailsComponent {
+  tabId = 1;
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: string
+    @Inject(MAT_DIALOG_DATA) public data: string,
+    public dialogRef: MatDialogRef<FieldDetailsComponent>
   ) {}
+
+  onChangeTab(tabId: number) {
+    this.tabId = tabId;
+  }
+
+  onDialogClose() {
+    this.dialogRef.close();
+  }
 }
