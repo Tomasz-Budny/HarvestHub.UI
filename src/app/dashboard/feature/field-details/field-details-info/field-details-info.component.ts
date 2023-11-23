d0f36b7d2ed139119c32fceb0fcf9bf47add314b
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-23 12:50:09 +0100

    Add field details info component

---
+++ src/app/dashboard/feature/field-details/field-details-info/field-details-info.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-field-details-info',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './field-details-info.component.html',
+  styleUrl: './field-details-info.component.scss'
+})
+export class FieldDetailsInfoComponent {
+
+}
