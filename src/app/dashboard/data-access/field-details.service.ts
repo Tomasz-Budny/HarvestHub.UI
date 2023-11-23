5c2050e2bcf0637c4fc98ddaf2d9da44b169ab39
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-23 18:19:10 +0100

    Add field details info skeleton animation

--- src/app/dashboard/data-access/field-details.service.ts
+++ src/app/dashboard/data-access/field-details.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { FieldDetailsViewModel } from '../data-model/field-details.model';
 import { HttpClient } from '@angular/common/http';
+import { Observable, Subject, switchMap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +10,16 @@ import { Observable } from 'rxjs';
 export class FieldDetailsService {
   URL = 'https://localhost:7258/api/fields/'
 
+
   constructor(
     public http: HttpClient
   ) { }
 
-  loadFieldDetails(fieldId: string): Observable<FieldDetailsViewModel> {
+  loadFieldDetails(fieldId: string) {
     return this.getFieldDetailsApi(fieldId);
   }
 
   private getFieldDetailsApi(fieldId: string) {
-    return this.http.get<FieldDetailsViewModel>(this.URL + `${fieldId}/details`)
+    return this.http.get<FieldDetailsViewModel>(this.URL + `${fieldId}/details`);
   }
 }
