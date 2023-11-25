13e164fa6946ec7fe0c7d53138606a9db90d3e26
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-25 12:14:42 +0100

    Add color picker component

---
+++ src/app/dashboard/feature/field-details/field-details-info/color-picker/color-picker.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-color-picker',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './color-picker.component.html',
+  styleUrl: './color-picker.component.scss'
+})
+export class ColorPickerComponent {
+
+}
