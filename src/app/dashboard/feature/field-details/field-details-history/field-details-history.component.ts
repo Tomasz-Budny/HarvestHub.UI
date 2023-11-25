3504d8b0314159d1368491539baa2838bb5d03a2
Tomasz-Budny <s208246@sggw.edu.pl>
2023-11-25 17:52:57 +0100

    Add harvest history record component

--- src/app/dashboard/feature/field-details/field-details-history/field-details-history.component.ts
+++ src/app/dashboard/feature/field-details/field-details-history/field-details-history.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HarvestHistoryRecordComponent } from '../../../ui/harvest-history-record/harvest-history-record.component';
 
 @Component({
   selector: 'app-field-details-history',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HarvestHistoryRecordComponent],
   templateUrl: './field-details-history.component.html',
   styleUrl: './field-details-history.component.scss'
 })
---
+++ src/app/dashboard/ui/harvest-history-record/harvest-history-record.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-harvest-history-record',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './harvest-history-record.component.html',
+  styleUrl: './harvest-history-record.component.scss'
+})
+export class HarvestHistoryRecordComponent {
+
+}
