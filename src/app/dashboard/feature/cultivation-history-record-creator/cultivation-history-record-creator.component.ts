4544b72b3f928dc9339bf1dd49190e500a8a4833
Tomasz-Budny <s208246@sggw.edu.pl>
2023-12-09 13:38:10 +0100

    Add cultivation history record creator component

---
+++ src/app/dashboard/feature/cultivation-history-record-creator/cultivation-history-record-creator.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cultivation-history-record-creator',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cultivation-history-record-creator.component.html',
+  styleUrl: './cultivation-history-record-creator.component.scss'
+})
+export class CultivationHistoryRecordCreatorComponent {
+
+}
